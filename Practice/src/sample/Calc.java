4d33b31b1ddb866c12fae855eeb78c54a89fd9c9
Shioriash <ashleyshiori.yoshida.y332@dirbato.co.jp>
2024-04-26 12:11:19 +0900

    Lesson 2

---
+++ Practice/src/sample/Calc.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class Calc {
+
+    public static void main(String[] args) {
+        var num1 = 1980;
+        var num2 = 9;
+        
+        //足し算
+        var result1= num1 + num2;
+        System.out.println("num1 + num2 =" + result1);
+
+    }
+
+}
